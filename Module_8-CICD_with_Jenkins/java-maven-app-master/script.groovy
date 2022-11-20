a8cc83ad40dcbec4f76052cdcacc57d66674392c
ngrandrei <negru.a13@gmail.com>
2022-11-20 18:22:47 +0200

    changed working directory for build image step

--- Module_8-CICD_with_Jenkins/java-maven-app-master/script.groovy
+++ Module_8-CICD_with_Jenkins/java-maven-app-master/script.groovy
@@ -3,11 +3,13 @@ def buildJar() {
 } 
 
 def buildImage() {
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    dir("./Module_8-CICD_with_Jenkins/java-maven-app-master/") {
+         withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         //note: usernamePassword() requires the credentials to be of the kind "username with password".
         sh "docker build -t negru1andrei/java-maven-app:2.0.0 ."
         sh "echo $PASS | docker login -u $USERNAME --password-stdin"
         sh "docker push negru1andrei/java-maven-app:2.0.0"
+        }
     }
 } 
 
