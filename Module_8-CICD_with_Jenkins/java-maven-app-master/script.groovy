7d0864ebcc0262bb726f4896eb36ad8f236fd266
ngrandrei <negru.a13@gmail.com>
2022-11-20 18:26:00 +0200

    change user env variable

--- Module_8-CICD_with_Jenkins/java-maven-app-master/script.groovy
+++ Module_8-CICD_with_Jenkins/java-maven-app-master/script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
          withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         //note: usernamePassword() requires the credentials to be of the kind "username with password".
         sh "docker build -t negru1andrei/java-maven-app:2.0.0 ."
-        sh "echo $PASS | docker login -u $USERNAME --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push negru1andrei/java-maven-app:2.0.0"
         }
     }
